30206ca731a07f15f6e6cff31f928f73ac6ede09
AlyHammoud <99489154+AlyHammoud@users.noreply.github.com>
2022-11-22 17:22:10 +0200

    add new resource api

--- server.cjs
+++ server.cjs
@@ -7,6 +7,7 @@ server.use(middlewares);
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
+	"/*": "/$1",
     "/api/*": "/$1",
     "/blog/:resource/:id/show": "/:resource/:id",
   })
